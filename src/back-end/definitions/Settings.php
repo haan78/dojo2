1013666bc2ca4f56849cfeaddfaa22114327d59a
haan78 <alibarisozturk@gmail.com>
2019-12-03 17:46:56 +0300

    vhjh

--- src/back-end/definitions/Settings.php
+++ src/back-end/definitions/Settings.php
@@ -1,5 +1,6 @@
 <?php
 define("PRJ_DATA_PATH","D:/code/dojo2/data");
+//define("PRJ_DATA_PATH","/var/data/dojo");
 define("SETTING_LOGFILE",PRJ_DATA_PATH . "/dojo.log");
 define("SETTING_DBFILE",PRJ_DATA_PATH."/dojo.db");
 define("SETTING_TITLE","Ankara Kendo Kulübü Yönetim Paneli");
