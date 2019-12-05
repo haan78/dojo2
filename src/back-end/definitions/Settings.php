342bce5ef87d5063381f7ac08590bc5eecffcb01
haan78 <alibarisozturk@gmail.com>
2019-12-05 10:08:28 +0300

    dfg

--- src/back-end/definitions/Settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("PRJ_DATA_PATH","D:/code/dojo2/data");
-//define("PRJ_DATA_PATH","/var/data/dojo");
-define("SETTING_LOGFILE",PRJ_DATA_PATH . "/dojo.log");
-define("SETTING_DBFILE",PRJ_DATA_PATH."/dojo.db");
-define("SETTING_TITLE","Ankara Kendo Kulübü Yönetim Paneli");
-define("SETTING_ID","WEBPACK - ANKARA_KENDO");
--- src/js/components/menu.vue
+++ src/js/components/menu.vue
@@ -10,7 +10,7 @@
           <a href="javascript:;"><b><i class="el-icon-tickets"></i>Raporlar</b></a>
           <ul>
             <li><a @click="link('/gelirgider')"><i class="el-icon-money" ></i>Gelir-Gider Dökümü</a></li>
-            <li><a @click="link('/geneluyeraporu')"><i class="el-icon-s-data" ></i>Genel Üye Raporu</a></li>
+            <li><a @click="link('/geneluyeraporu')"><i class="el-icon-user-solid" ></i>Genel Üye Raporu</a></li>
             <li><a @click="link('/geneldurum')"><i class="el-icon-s-data" ></i>Genel Durum</a></li>
           </ul>
         </li>
