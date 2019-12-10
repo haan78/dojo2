d2b2a0ce9dc1d4e2a0bd4fdd512de12d782970f5
haan78 <alibarisozturk@gmail.com>
2019-12-10 13:49:23 +0300

    gh

--- src/index.php
+++ src/index.php
@@ -1,16 +1,14 @@
 <?php 
 
-/* Project Settings */
 if ( file_exists( "/var/data/dojo" ) ) {
     define("SETTING_DATA_PATH","/var/data/dojo"); //Product Machine
 } else {
     define("SETTING_DATA_PATH",__DIR__."/../data"); //Developer Machine
 }
-define("SETTING_LOGFILE",SETTING_DATA_PATH . "/dojo.log");
-define("SETTING_DBFILE",SETTING_DATA_PATH."/dojo.db");
-define("SETTING_TITLE","Ankara Kendo Kulübü Yönetim Paneli");
-define("SETTING_PAGE_LOADING","Yükleniyor Lütfen Bekleyin...");
-define("SETTING_ID","WEBPACK - ANKARA_KENDO");
-
-require_once __DIR__ . "/back-end/router.php";
-new router( (isset($_GET["a"]) ? trim($_GET["a"]) : "application" ) );
+if ( defined("SETTING_DATA_PATH") && file_exists(SETTING_DATA_PATH."/settings.php") ) {
+    require_once SETTING_DATA_PATH."/settings.php";
+    require_once __DIR__ . "/back-end/router.php";
+    new router( (isset($_GET["a"]) ? trim($_GET["a"]) : "application" ) );
+} else {
+    die("Configuration Error!");
+}
