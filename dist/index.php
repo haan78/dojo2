dadba3a099490dff4bffae761bcf1c1ec48c40d6
haan78 <alibarisozturk@gmail.com>
2019-12-12 17:13:20 +0300

    Docker entegrasyonu

--- dist/index.php
+++ dist/index.php
@@ -1,11 +1,7 @@
 <?php 
 
-if ( file_exists( "/var/data/dojo" ) ) {
-    define("SETTING_DATA_PATH","/var/data/dojo"); //Product Machine
-} else {
-    define("SETTING_DATA_PATH",__DIR__."/../data"); //Developer Machine
-}
-if ( defined("SETTING_DATA_PATH") && file_exists(SETTING_DATA_PATH."/settings.php") ) {
+define("SETTING_DATA_PATH","/var/data/dojo");
+if ( file_exists(SETTING_DATA_PATH."/settings.php") ) {
     require_once SETTING_DATA_PATH."/settings.php";
     require_once __DIR__ . "/back-end/router.php";
     new router( (isset($_GET["a"]) ? trim($_GET["a"]) : "application" ) );
