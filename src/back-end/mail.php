317b0e7b146baabc92fef5e998ffdc749fd2772c
haan78 <alibarisozturk@gmail.com>
2019-12-23 17:43:03 +0300

    c

--- src/back-end/mail.php
+++ src/back-end/mail.php
@@ -1,20 +1,34 @@
 <?php
 
+require_once __DIR__ . "/PHPMailer/src/PHPMailer.php";
+require_once __DIR__ . "/PHPMailer/src/SMTP.php";
+require_once __DIR__ . "/PHPMailer/src/Exception.php";
+
+
+use \PHPMailer\PHPMailer\PHPMailer;
+
 class Mail {
     public static function smtp() {
         $mail = new PHPMailer();
     
         $mail->IsSMTP();
-        $mail->SMTPDebug = 0;
+        $mail->SMTPDebug = 2;
         $mail->CharSet = "UTF-8";
     
         $mail->Host = "tls://smtp.gmail.com:587";
-        $mail->Username = "ankarakendoiaido@gmail.com";
-        $mail->Password = "";
+        /*$mail->SMTPOptions = array(
+            'ssl' => array(
+              'verify_peer' => false,
+              'verify_peer_name' => false,
+              'allow_self_signed' => true
+             )
+         );*/
+        $mail->Username = "ankarakendo2007@gmail.com";
+        $mail->Password = "akendo@@77";
         $mail->SMTPAuth = true;
     
         $mail->From = $mail->Username;
-        $mail->FromName = 'Rehber Eczanem Sistemi';
+        $mail->FromName = 'Ankara Kendo Kulübü';
         $mail->IsHTML(true);
         return $mail;
     }
